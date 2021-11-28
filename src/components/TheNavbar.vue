c9f82e90b59dd5439488c3b0492ae25a24585520
Yura Larsen <hel1_yeah@ukr.net>
2021-11-28 13:54:50 +0200

    feat: vue-router. components is,

---
+++ src/components/TheNavbar.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="navbar">
+    <h3>Online Bank</h3>
+    <ul class="navbar-menu">
+      <li class="navbar-menu__item">
+        <router-link to="/" class="navbar-menu__item-link"
+          >applications</router-link
+        >
+      </li>
+      <li class="navbar-menu__item">
+        <router-link to="/">help</router-link>
+      </li>
+      <li class="navbar-menu__item">
+        <a href="#">messages</a>
+      </li>
+      <li class="navbar-menu__item">
+        <a href="#">exit</a>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+.navbar-menu__item-link {
+  text-transform: uppercase;
+}
+</style>
---
+++ src/layout/MainLayout.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="container with-nav">
+    <div class="card">
+      <the-navbar></the-navbar>
+    </div>
+  </div>
+</template>
+
+<script>
+import TheNavbar from './../components/TheNavbar.vue';
+export default {
+  components: {
+    TheNavbar,
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
